18db735b9c74cdd6ef3742eb1b31d2e4d471035d
parthi14199 <parthi14199@gmail.com>
2023-04-14 10:44:16 +0530

    ok

---
+++ DAY 1/Checking.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+s = Service("C:\\Selenium\\chromedriver.exe")
+driver = webdriver.Chrome(service=s)
+driver.get("https://demo.nopcommerce.com/register")
+driver.maximize_window()
+
+sbox=driver.find_element(By.XPATH,"//input[@id='small-searchterms']")
+print("Display status:",sbox.is_displayed())
+print("Enabled status:",sbox.is_enabled())
+
+driver.close()
