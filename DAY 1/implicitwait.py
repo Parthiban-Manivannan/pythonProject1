fb2d8b4e788180486aa90dd297b71e31d57254bf
parthi14199 <parthi14199@gmail.com>
2023-04-14 11:50:33 +0530

    ok

--- DAY 1/implicitwait.py
+++ DAY 1/implicitwait.py
@@ -4,10 +4,12 @@ from selenium.webdriver.common.by import By
 
 s = Service("C:\\Selenium\\chromedriver.exe")
 driver = webdriver.Chrome(service=s)
-driver.implicitly_wait(20)
+
 driver.get("https://www.google.com")
 driver.maximize_window()
 sbox=driver.find_element(By.NAME,'q')
+
 sbox.send_keys("Selenium")
 sbox.submit()
+driver.implicitly_wait(20)
 driver.find_element(By.XPATH,"//h3[text()='Selenium' and @Class='LC20lb MBeuO DKV0Md']").click()
