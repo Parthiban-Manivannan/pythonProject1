e174aac5e9a52dd472b7e10fdc4251e0b00b17f4
parthi14199 <parthi14199@gmail.com>
2024-02-18 12:59:41 +0530

    2/9/2023

--- DAY 1/YouTube.py
+++ DAY 1/YouTube.py
@@ -1,16 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 
 def youtube_search(query):
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(options=options)
     driver.get("https://www.youtube.com")
-    search_box = driver.find_element_by_name("search_query")
-    search_box.send_keys(query)
+    driver.maximize_window()
+    search_box = driver.find_element(By.XPATH, "//input[@id='search']")
+    try:
+        search_box.send_keys("How to automate YouTube with Python")
+    except:
+        print('Failed')
     search_box.send_keys(Keys.ENTER)
-    first_video = driver.find_element_by_id("video-title")
+    first_video = driver.find_element(By.ID,"video-title")
+    first_video.click()
     return first_video.text
 
 if __name__ == "__main__":
     print(youtube_search("How to automate YouTube with Python"))
+
+
